0c683e3ea8ba48488ff057ab8bcdede8fa937b0c
Juhani Sankari <juhani.sankari@hotmail.fi>
2022-02-06 11:07:34 +0200

    Fixed routing problems and added cooperative transmission lines

--- simulator.py
+++ simulator.py
@@ -1,4 +1,5 @@
 
+from logging import exception
 from tkinter import Y
 from turtle import color, delay, update
 import numpy
@@ -19,7 +20,7 @@ number_of_nodes = 60
 nodes = []
 area = 1000
 min_dist = 50
-radio_range = 200
+radio_range = 250
 
 
 class Node:
@@ -56,24 +57,31 @@ def transmit(transmitter,message):
  
     for node2 in nodes:
         if in_range(transmitter,node2):
-            for recieved_message in node2.messages:
-                if message.index == recieved_message.index:
-                    if message.hops == recieved_message.hops:
-                        #plt.plot([transmitter.x,node2.x],[transmitter.y,node2.y],color = colors[4])
-                        pass
-                
-                
-            if len(node2.messages)==0:
+            if len(node2.messages) == 0:
                 plt.plot([transmitter.x,node2.x],[transmitter.y,node2.y],color = colors[message.hops%len(colors)])
+                #plt.plot([transmitter.x,node2.x],[transmitter.y,node2.y],color = colors[message.index])
                 new_message=copy.copy(message)
                 new_message.hops = message.hops +1
                 node2.messages.append(new_message)
-                
-            
 
-#Place nodes to the area with minimum distance in between
-i=0
-while i < number_of_nodes:
+            if not any(obj.index == message.index for obj in node2.messages):
+                plt.plot([transmitter.x,node2.x],[transmitter.y,node2.y],color = colors[message.hops%len(colors)])
+                #plt.plot([transmitter.x,node2.x],[transmitter.y,node2.y],color = colors[message.index])
+                new_message=copy.copy(message)
+                new_message.hops = message.hops +1
+                node2.messages.append(new_message)
+
+
+            if any(obj.index == message.index and obj.hops == message.hops+1 for obj in node2.messages):
+                plt.plot([transmitter.x,node2.x],[transmitter.y,node2.y],color = colors[message.hops%len(colors)])
+                #plt.plot([transmitter.x,node2.x],[transmitter.y,node2.y],color = colors[message.index])
+
+
+                
+               
+
+
+for i in range(number_of_nodes):
     y = random.randint(0,area)
     x = random.randint(0,area)
     try:
@@ -100,13 +108,14 @@ for node in nodes:
     #ax.add_patch(circle)
 
 #plot lines reprecenting channels between nodes
-
-
-message = Message(0,1,0)
-message2 = Message(1,2,0)
+message = Message(0,0,0)
+message2 = Message(1,1,0)
 nodes[2].messages.append(message)
-circle = plt.Circle((nodes[2].x,nodes[2].y),min_dist,fill = False)
-ax.add_patch(circle)
+#nodes[3].messages.append(message2)
+circle1 = plt.Circle((nodes[2].x,nodes[2].y),min_dist,fill = False)
+circle2 = plt.Circle((nodes[3].x,nodes[3].y),min_dist,fill = False)
+ax.add_patch(circle1)
+ax.add_patch(circle2)
 #nodes[6].messages.append(message2)
 for i in range(1,5):
     update()
