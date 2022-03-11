7809ad33b5b77cfac0672f03ebdf02b93d731e84
Juhani Sankari <juhani.sankari@hotmail.fi>
2022-03-11 11:14:27 +0200

    Added TODO list

--- simulator.py
+++ simulator.py
@@ -1,9 +1,18 @@
+#TODO:
+#Kahden vyöryn törmäys => kumpaakaan ei vastaanoteta
+#Kaksi identtistä lähetystä => summautuva lähetysteho
+#Vaimenemismalli 
+#Multisource odotus. Jos vastaanottaa, ei lähetä
+#Multisource tavat
+#-1.Lähde odottaa nykyisen viestin leviämistä koko verkkoon
+#-2.Spatial reuse (2014 artikkeli)
+#Unicast CBR
 
 from cProfile import label
 from logging import exception
 from tkinter import Y
 from turtle import color, delay, update, width
-import numpy
+import numpy as np
 import math
 import copy
 import matplotlib.pyplot as plt
@@ -19,9 +28,10 @@ colors = cmap.colors  # type: list
 
 number_of_nodes = 100
 nodes = []
-area = 1500
-min_dist = 50
-radio_range = 400
+area = 600
+min_dist = 20
+radio_range = 100
+M = 3
 
 
 class Node:
@@ -54,39 +64,38 @@ def in_range(node1,node2):
         return False
 
 def draw_arrow(tx,rx,color_index):
-   offset = 2
-   dist = (math.sqrt((tx.x-tx.x)**2+(tx.y-rx.y)**2))
-   dx =  (rx.x-tx.x)
-   dy =  (rx.y-tx.y)
+   offset = 0
+   dx1 =(rx.x-tx.x)
+   dy1 =(rx.y-tx.y)
+   alpha = math.atan2(dy1,dx1)
+   if alpha < 0:
+       alpha += 2*math.pi
+
+   dx =  dx1-math.cos(alpha)*offset*np.sign(rx.x-tx.x)
+   dy =  dy1-math.sin(alpha)*offset*np.sign(rx.y-tx.y)
    #dy = end_y-tx.y
    #dx = end_x-tx.x
-   plt.arrow(tx.x,tx.y,dx,dy,head_width = area/200,color = colors[color_index%len(colors)],length_includes_head = True)
+   plt.arrow(tx.x,tx.y,dx,dy,head_width = area/150,color = colors[color_index%len(colors)],length_includes_head = True)
 
 def transmit(transmitter,message):
  
     for node2 in nodes:
         if in_range(transmitter,node2):
             if len(node2.messages) == 0:
-                #plt.plot([transmitter.x,node2.x],[transmitter.y,node2.y],color = colors[message.hops%len(colors)])
-                #plt.plot([transmitter.x,node2.x],[transmitter.y,node2.y],color = colors[message.index])
-                draw_arrow(transmitter,node2,message.hops)
+                draw_arrow(transmitter,node2,message.hops%M)
                 new_message=copy.copy(message)
                 new_message.hops = message.hops +1
                 node2.messages.append(new_message)
 
             if not any(obj.index == message.index for obj in node2.messages):
-                #plt.plot([transmitter.x,node2.x],[transmitter.y,node2.y],color = colors[message.hops%len(colors)])
-                #plt.plot([transmitter.x,node2.x],[transmitter.y,node2.y],color = colors[message.index])
-                draw_arrow(transmitter,node2,message.hops)
+                draw_arrow(transmitter,node2,message.hops%M)
                 new_message=copy.copy(message)
                 new_message.hops = message.hops +1
                 node2.messages.append(new_message)
 
 
             if any(obj.index == message.index and obj.hops == message.hops+1 for obj in node2.messages):
-                #plt.plot([transmitter.x,node2.x],[transmitter.y,node2.y],color = colors[message.hops%len(colors)])
-                #plt.plot([transmitter.x,node2.x],[transmitter.y,node2.y],color = colors[message.index])
-                draw_arrow(transmitter,node2,message.hops)
+                draw_arrow(transmitter,node2,message.hops%M)
                 pass
 
 
@@ -123,20 +132,21 @@ for node in nodes:
 
 #plot lines reprecenting channels between nodes
 message = Message(0,0,0)
-message2 = Message(1,1,0)
+#message2 = Message(0,1,0)
 nodes[2].messages.append(message)
 #nodes[3].messages.append(message2)
-#circle1 = plt.Circle((nodes[2].x,nodes[2].y),min_dist,fill = False)
+circle1 = plt.Circle((nodes[2].x,nodes[2].y),min_dist,fill = False)
 #circle2 = plt.Circle((nodes[3].x,nodes[3].y),min_dist,fill = False)
-#ax.add_patch(circle1)
+ax.add_patch(circle1)
 #ax.add_patch(circle2)
-#nodes[6].messages.append(message2)
-for i in range(0,3):
+for i in range(0,10):
     update(i)
-plt.xlim([0,area])
-plt.ylim([0,area])
+
+plt.xlim(0,area)
+plt.ylim(0,area)
 plt.show()
 
+    
 
 
 
